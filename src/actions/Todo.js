b3a47764da26f3983bdd91b9cbed9b4112569a69
jun68ykt <jun68.ykt@gmail.com>
2019-05-05 13:26:07 +0900

    省略できるコードを削除

--- src/actions/Todo.js
+++ src/actions/Todo.js
@@ -2,14 +2,14 @@ export const addTodo = (todo) => {
   return {
     type: 'ADD_TODO',
     // id: nextTodoId++,
-    payload: { todo: todo }
+    payload: { todo }
   };
 }
 
 export const delTodo = (todo) => {
   return {
     type: 'DEL_TODO',
-    payload: { todo: todo }
+    payload: { todo }
   };
 }
 
